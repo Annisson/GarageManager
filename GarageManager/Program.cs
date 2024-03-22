00ba9293ca2d1d4f5ec31e1406e347e6da72d6d4
Annica Andersson <annicaandersson@live.se>
2024-03-22 15:55:20 +0100

    Add Airplane class

--- GarageManager/Program.cs
+++ GarageManager/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace GarageManager
+﻿using GarageManager.Vehicles;
+
+namespace GarageManager
 {
     internal class Program
     {
@@ -6,45 +8,63 @@
         {
             Console.WriteLine("Welcome to my ~*~ Garage ~*~");
 
-            while (true)
-            {
-                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2 or 0) of your choice"
-                    + "\n1. "
-                    + "\n2. "
-                    + "\n0. Exit the application");
-                char input = ' '; //Creates the character input to be used with the switch-case below.
-                try
-                {
-                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
-                    Console.Clear();
-                }
-                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
-                {
-                    Console.Clear();
-                    Console.WriteLine("Please enter some input!");
-                }
-                switch (input)
-                {
-                    case '1':
 
-                        break;
-                    case '2':
 
-                        break;
-                    case '3':
+            // TODO: Testar, tas bort
 
-                        break;
-                    case '4':
+            var car1 = new Car(123, "red", 4, Car.FuelType.Gasoline); 
+            var mc = new Motorcycle(21, "black", 2, Motorcycle.EngineType.Rotary, false);
+            var plane1 = new Airplane(24, "white", 4, 2, 35.79);
 
-                        break;
-                    case '0':
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Please enter some valid input (1, 2, or 0)");
-                        break;
-                }
-            }
+            Console.WriteLine(car1.VehicleInformation()); 
+            Console.WriteLine(mc.VehicleInformation());
+            Console.WriteLine(plane1.VehicleInformation());
+
+
+
+            // ToDo: Ta bort hit
+
+
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2 or 0) of your choice"
+            //        + "\n1. "
+            //        + "\n2. "
+            //        + "\n0. Exit the application");
+            //    char input = ' '; //Creates the character input to be used with the switch-case below.
+            //    try
+            //    {
+            //        input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+            //        Console.Clear();
+            //    }
+            //    catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+            //    {
+            //        Console.Clear();
+            //        Console.WriteLine("Please enter some input!");
+            //    }
+            //    switch (input)
+            //    {
+            //        case '1':
+
+            //            break;
+            //        case '2':
+
+            //            break;
+            //        case '3':
+
+            //            break;
+            //        case '4':
+
+            //            break;
+            //        case '0':
+            //            Environment.Exit(0);
+            //            break;
+            //        default:
+            //            Console.WriteLine("Please enter some valid input (1, 2, or 0)");
+            //            break;
+            //    }
+            //}
         }
     }
 }
---
+++ GarageManager/Vehicles/Airplane.cs
@@ -0,0 +1,18 @@
+﻿namespace GarageManager.Vehicles
+{
+    internal class Airplane : Vehicle
+    {
+        public int NumberOfEngines { get; set; }
+        public double Wingspan { get; set; }
+        public Airplane(uint vehicleID, string color, int numberOfWheels, int numberOfEngines, double wingspan) : base(vehicleID, color, numberOfWheels)
+        {
+            NumberOfEngines = numberOfEngines;
+            Wingspan = wingspan;
+        }
+
+        public override string VehicleInformation()
+        {
+            return base.VehicleInformation() + $", NumberOfEngines: {NumberOfEngines}, Wingspan: {Wingspan}";
+        }
+    }
+}
