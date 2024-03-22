89d2c8944a4c10907688b3744393218fbbf52160
Annica Andersson <annicaandersson@live.se>
2024-03-22 15:43:16 +0100

    Add Motorcycle class

---
+++ GarageManager/Vehicles/Motorcycle.cs
@@ -0,0 +1,26 @@
+﻿namespace GarageManager.Vehicles
+{
+    internal class Motorcycle : Vehicle
+    {
+        public enum EngineType
+        {
+            InlineFour,
+            Vtwin,
+            SingleCylinder,
+            Rotary,
+            Boxer
+        }
+        public EngineType MotorcycleEngine { get; set; }
+        public bool HasSidecar { get; set; }
+        public Motorcycle(uint vehicleID, string color, int numberOfWheels, EngineType motorcycleEngine, bool hasSideCar) : base(vehicleID, color, numberOfWheels)
+        {
+            MotorcycleEngine = motorcycleEngine;
+            HasSidecar = hasSideCar;
+        }
+
+        public override string VehicleInformation()
+        {
+            return base.VehicleInformation() + $", MotorcycleEngine: {MotorcycleEngine}, HasSidecar: {(HasSidecar ? "Yes" : "No")}";
+        }
+    }
+}
