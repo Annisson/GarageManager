44e8042c6127eb0652e5414a03dbe7361cf3d946
Annica Andersson <annicaandersson@live.se>
2024-03-22 09:54:02 +0100

    Add menu shell

--- GarageManager/Program.cs
+++ GarageManager/Program.cs
@@ -5,6 +5,46 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my ~*~ Garage ~*~");
+
+            while (true)
+            {
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2 or 0) of your choice"
+                    + "\n1. "
+                    + "\n2. "
+                    + "\n0. Exit the application");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                    Console.Clear();
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+
+                        break;
+                    case '2':
+
+                        break;
+                    case '3':
+
+                        break;
+                    case '4':
+
+                        break;
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (1, 2, or 0)");
+                        break;
+                }
+            }
         }
     }
 }
