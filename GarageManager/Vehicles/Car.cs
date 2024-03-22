f9da0260989296cdd8462abb35c6c5db8304b71c
Annica Andersson <annicaandersson@live.se>
2024-03-22 14:41:46 +0100

    Add Car class

---
+++ GarageManager/Vehicles/Car.cs
@@ -0,0 +1,26 @@
+﻿using static GarageManager.Vehicles.Car;
+
+namespace GarageManager.Vehicles
+{
+    internal class Car : Vehicle
+    {
+        public enum FuelType
+        {
+            Gasoline,
+            Diesel,
+            Electric,
+            Hybrid,
+            Hydrogen
+        }
+        public FuelType CarFuelType { get; set; }
+        public Car(uint vehicleID, string color, int numberOfWheels, FuelType fuelType) : base(vehicleID, color, numberOfWheels)
+        {
+            CarFuelType = fuelType;
+        }
+
+        public virtual string VehicleInformation()
+        {
+            return base.VehicleInformation() + $", FuelType: {CarFuelType}.";
+        }
+    }
+}
