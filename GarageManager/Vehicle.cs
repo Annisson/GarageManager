74809c310acffd7ef3810d0d1b40fd18b4209ee2
Annica Andersson <annicaandersson@live.se>
2024-03-22 11:56:18 +0100

    Add Vehicle baseclass

---
+++ GarageManager/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Xml.Linq;
+
+namespace GarageManager
+{
+    // Fordonsklass, basklass för alla fordon
+    internal abstract class Vehicle
+    {
+        public uint VehicleID { get; private set; } // ToDo: Egen klass för att skapa detta?
+        public string Color { get; set; }
+        public int NumberOfWheels { get; set; }
+
+        protected Vehicle(uint vehicleID, string color, int numberOfWheels)
+        {
+            VehicleID = vehicleID;
+            Color = color;
+            NumberOfWheels = numberOfWheels;
+        }
+
+        public virtual string VehicleInformation()
+        {
+            return $"VehicleID: {VehicleID}, Color: {Color}, NumberOfWheels: {NumberOfWheels}";
+        }
+
+
+    }
+}
