bfa56696f52097794156083c8dbf7656e12b3204
Annica Andersson <annicaandersson@live.se>
2024-03-23 12:50:23 +0100

    Add Bus class

---
+++ GarageManager/Vehicles/Bus.cs
@@ -0,0 +1,16 @@
+﻿namespace GarageManager.Vehicles
+{
+    internal class Bus : Vehicle
+    {
+        public int NumberOfSeats { get; set; }
+        public Bus(uint vehicleID, string color, int numberOfWheels, int numberOfSeats) : base(vehicleID, color, numberOfWheels)
+        {
+            NumberOfSeats = numberOfSeats;
+        }
+
+        public override string VehicleInformation()
+        {
+            return base.VehicleInformation() + $", NumberOfSeats: {NumberOfSeats}";
+        }
+    }
+}
