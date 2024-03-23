6f0fd854d58b920c5bde4c6bf92ef891bcebb777
Annica Andersson <annicaandersson@live.se>
2024-03-23 12:54:20 +0100

    Add Boat class

---
+++ GarageManager/Vehicles/Boat.cs
@@ -0,0 +1,16 @@
+﻿namespace GarageManager.Vehicles
+{
+    internal class Boat : Vehicle
+    {
+        public double Lenght { get; set; }
+        public Boat(uint vehicleID, string color, int numberOfWheels, double lenght) : base(vehicleID, color, numberOfWheels)
+        {
+            Lenght = lenght;
+        }
+
+        public override string VehicleInformation()
+        {
+            return base.VehicleInformation() + $", Lenght: {Lenght}m";
+        }
+    }
+}
