f4ee036fcc97801489d5fc9bfd3f64f3c6d0850f
Annica Andersson <annicaandersson@live.se>
2024-03-24 02:52:46 +0100

    Add Garage class

---
+++ GarageManager/Garage/Garage.cs
@@ -0,0 +1,29 @@
+﻿using GarageManager.Vehicles;
+using System.Collections;
+
+namespace GarageManager.Garage
+{
+    internal class Garage<T> : IEnumerable<T> where T : Vehicle 
+    {
+        private T[] vehicles; // Privat array för samling av fordon
+
+        public Garage(int capacity) // Konstruktor som tar in kapaciteten som argument
+        {
+            vehicles = new T[capacity];
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T vehicle in vehicles) 
+            {
+                yield return vehicle; 
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
