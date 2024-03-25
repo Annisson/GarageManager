1b6e243dc9458e0b658df9d32e9dbdee7f84e6e5
Annica Andersson <annicaandersson@live.se>
2024-03-25 16:41:50 +0100

    Add method to park a new vehicle

--- GarageManager/Garage/Garage.cs
+++ GarageManager/Garage/Garage.cs
@@ -12,18 +12,41 @@ namespace GarageManager.Garage
             vehicles = new T[capacity];
         }
 
+        public void AddVehicle(T vehicle)
+        {
+            int index = Array.FindIndex(vehicles, x => x == null); // Hittar första plats(index) i arrayen so är null, dvs första lediga plats
+
+            if (index != -1) // FindIndex returnerar -1 om den inte hittar en ledig plats, så om index inte är -1 så tilldelas första bästa plats nedan
+            {
+                vehicles[index] = vehicle; // Tilldelar arrayen den platsen som var ledig
+                Console.WriteLine($"\nThe {vehicle.GetType().Name} with ID-number \"{vehicle.VehicleID}\" has been added to the garage.");
+            }
+            else
+            {
+                Console.WriteLine("The garage is full, you cannot add any more vehicles.");
+            }
+        }
+
+        // Metod att kalla på för att lägga till vehicles
+
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+
+        }
+
+        // Metod för att ta bort vehicles
+
+
 
         public IEnumerator<T> GetEnumerator()
         {
             foreach (T vehicle in vehicles) 
             {
-                yield return vehicle; 
+                yield return vehicle; // Returnerar en vehicle åt gången
             }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
     }
 }
--- GarageManager/Garage/GarageHandler.cs
+++ GarageManager/Garage/GarageHandler.cs
@@ -1,8 +1,71 @@
-﻿namespace GarageManager.Garage
+﻿using GarageManager.Helpers;
+using GarageManager.Vehicles;
+
+namespace GarageManager.Garage
 {
     internal class GarageHandler
     {
         //  ingen kontakt mellan Garage och UI, allt hamnar här
+
+        Garage<Vehicle> newGarage = new(10);
+
+        public void ParkVehicle()
+        {
+            Console.WriteLine("\nPlease enter information below about the vehicle you want to park");
+
+            string type = Util.GetVehicleTypeInput(); // Vilket typ av vehicle användaren vill registrera
+            string color = Util.GetStringInput("Color: ");
+            int numberOfWheels = Util.GetIntInput("Number of Wheels: ");
+
+            Vehicle vehicle = CreateVehicle(type, color, numberOfWheels); // Kallar på createVehicle metoden och skickar in det användaren valt som gäller för alla vehicles
+
+            if (vehicle != null)
+            {
+                newGarage.AddVehicle(vehicle); // Lägg till i garaget
+                Console.WriteLine(vehicle.VehicleInformation()); // Skriver ut alla egenskaper
+            }
+            else
+            {
+                Console.WriteLine("Invalid vehicle type.");
+            }
+            Console.WriteLine("\nPress Enter to continue..."); // Paus för att inte hoppa direkt till menyn igen
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        private Vehicle CreateVehicle(string type, string color, int numberOfWheels)
+        {
+            switch (type)
+            {
+                case "1": // Airplane
+                    int numberOfEngines = Util.GetIntInput("Number of engines: ");
+                    double wingspan = Util.GetDoubleInput("Wingspan in meters: ");
+                    return new Airplane(color, numberOfWheels, numberOfEngines, wingspan);
+
+                case "2": // Boat
+                    double length = Util.GetDoubleInput("Length of the boat in meters: ");
+                    return new Boat(color, numberOfWheels, length);
+
+                case "3": // Bus
+                    int numberOfSeats = Util.GetIntInput("Number of seats: ");
+                    return new Bus(color, numberOfWheels, numberOfSeats);
+
+                case "4": // Car
+                    int previousOwners = Util.GetIntInput("Number of previous owners: ");
+                    return new Car(color, numberOfWheels, previousOwners);
+
+                case "5": // Motorcycle
+                    double cylindervolume = Util.GetDoubleInput("Cylindervolume: ");
+                    return new Motorcycle(color, numberOfWheels, cylindervolume);
+
+                default:
+                    return null!; // Släcker null-varningen eftersom det hanteras i ParkVehicle metoden
+            }
+        }
+
+
+
+
 
     }
 }
