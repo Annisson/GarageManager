9c3c6ea5409ed47669de3be6a1e793989f6808d7
Annica Andersson <annicaandersson@live.se>
2024-03-25 20:59:12 +0100

    Add Manager class

---
+++ GarageManager/Manager.cs
@@ -0,0 +1,16 @@
+﻿using GarageManager.UserInterface;
+
+namespace GarageManager
+{
+    internal class Manager
+    {
+        internal static void Run()
+        {
+            Console.WriteLine("~*~ Welcome to my Garage ~*~\n");
+
+            ConsoleUI.MainMenu();
+
+        }
+
+    }
+}
--- GarageManager/Program.cs
+++ GarageManager/Program.cs
@@ -1,16 +1,11 @@
-﻿using GarageManager.UserInterface;
-using GarageManager.Vehicles;
-
-namespace GarageManager
+﻿namespace GarageManager
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to my ~*~ Garage ~*~");
-            Console.WriteLine();
-            ConsoleUI.MainMenu();
-
+            var manager = new Manager();
+            Manager.Run();
         }
     }
 }
