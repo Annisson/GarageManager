668f23dcbaa130bfc6e73f10dac13fb3adf39e1b
Annica Andersson <annicaandersson@live.se>
2024-03-27 01:48:26 +0100

    Add XUnitTest project

---
+++ GarageManager.Tests/UnitTests.cs
@@ -0,0 +1,32 @@
+namespace GarageManager.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void AddVehicle_ShouldAddVehicleToList()
+        {
+
+            // Arrange
+
+
+
+            // Act
+
+
+
+
+            //Assert
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
