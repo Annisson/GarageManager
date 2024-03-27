0a61241458c1754a513cfd1d6e3b818a041d3426
Annica Andersson <annicaandersson@live.se>
2024-03-27 10:33:48 +0100

    Add xUnitTest AddVehicle_ShouldAddVehicleToGarage

--- GarageManager.Tests/UnitTests.cs
+++ GarageManager.Tests/UnitTests.cs
@@ -1,32 +1,24 @@
+using GarageManager.Vehicles;
+using GarageManager.Garage;
+
 namespace GarageManager.Tests
 {
     public class UnitTests
     {
         [Fact]
-        public void AddVehicle_ShouldAddVehicleToList()
+        public void AddVehicle_ShouldAddVehicleToGarage()
         {
-
             // Arrange
-
-
+            int capacity = 5; // Simulerar 5st platser i garaget
+            var garage = new Garage<Vehicle>(capacity); // Instansierar nytt Garage med platserna ovan
+            var vehicleToAdd = new Airplane("White", 4, 2, 35.79); // Simulerar ett fordon(Airplane) som ska lägags till
 
             // Act
-
-
-
+            garage.AddVehicle(vehicleToAdd);
 
             //Assert
-
-
-
-
+            Assert.Contains(vehicleToAdd, garage); // Kollar att förvantade utgång(flygplanet), stämmer med den faktiskta utgången
         }
-
-
-
-
-
-
 
     }
 }
